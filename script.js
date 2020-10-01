310a580683a17e1ac8bc2bc31953ba9a4dbc659f
Jisun Yang <54940827+jisunya@users.noreply.github.com>
2020-10-01 22:40:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,40 @@ window.onscroll = () => {
   sticky();
 }
 
-let target = document.querySelector(".menu");
-let targetTop = target.offsetTop;
+/* 
+# Create Dummy Text #
+It is to create dummy paragraph instead of writing repetitive DOM elements in HTML.
+You don't have to use the code below in your project.
+In this example, however, you have to declare them first before declare "function sticky()".
+*/
+
+let parag1 = document.querySelector(".parag1");
+let parag2 = document.querySelector(".parag2");
+
+function createDummyParag(parag, line) {
+  let sum ='';
+  let txt = "Elit velit exercitation est commodo. "
+  for (let i = 0; i < line; i++){
+    sum += txt;
+  }
+  parag.innerHTML = "<h3>" + txt + "</h3>" + "<p>" + sum + "</p>";
+  sum = '';
+}
+
+createDummyParag(parag1, 100);
+createDummyParag(parag2, 300);
+
 
 function sticky() {
-  if (window.pageYOffset > targetTop) {
+  let target = document.querySelector(".menu");
+  let targetTop = target.offsetTop;
+  let absoluteTargetTop = target.previousElementSibling.offsetHeight;
+
+  if (window.pageYOffset >= targetTop) {
     target.classList.add("sticky");
+    if (window.pageYOffset <= absoluteTargetTop) {
+      target.classList.remove("sticky");
+    }
   } else {
     target.classList.remove("sticky");
   }
